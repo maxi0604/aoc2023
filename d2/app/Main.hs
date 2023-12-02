6eedcf729cdb80644c642ca80e993d5681d67570
Maximilian R. <maxi.rostock@outlook.de>
2023-12-02 17:50:49 +0100

    Actually do the task

--- d2/app/Main.hs
+++ d2/app/Main.hs
@@ -8,15 +8,34 @@ main :: IO ()
 main = do
     content <- readFile "/dev/stdin"
     let linesOfFile = lines content
-    let parsed = map parseLine linesOfFile
+    let lexed = map lexLine linesOfFile
+    print lexed
+    let parsed = map parseLine lexed
+    print parsed
+    --
     print $ sumLegal parsed
     return ()
 
+-- All elements fulfil condition and at least one exists.
+all' :: (a -> Bool) -> [a] -> Bool
+all' f x = all f x && not (null x)
 
-parseLine :: String -> Game
-parseLine = chunksOf 3 . map read . tail . splitWhen (not . isDigit)
+parseLine :: RawGame -> Game
+parseLine = map parseTriple
+
+lexLine :: String -> RawGame
+lexLine = map (map (splitOn " ")) . map (splitOn ", ") . splitOn "; " . last . splitOn ": "
+
+parseTriple :: [[String]] -> [Int]
+parseTriple = map sum . transpose . map parsePair
+
+parsePair :: [String] -> [Int]
+parsePair [r, "red"] = [read r, 0, 0]
+parsePair [g, "green"] = [0, read g, 0]
+parsePair [b, "blue"] = [0, 0, read b]
 
 type Game = [[Int]]
+type RawGame = [[[String]]]
 
 maxLegal :: Game -> [Int]
 maxLegal = map maximum . transpose
