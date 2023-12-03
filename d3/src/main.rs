0e08babace5fc8c0b5818afb204167488076e652
Maximilian R. <maxi.rostock@outlook.de>
2023-12-03 14:48:43 +0100

    Start working on part 2

--- d3/src/main.rs
+++ d3/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, str, cmp::{max, min}};
+use std::{io, str, cmp::{max, min}, collections::HashMap};
 
 fn main() {
     let mut arr: Vec<Vec<u8>> = io::stdin().lines().map(|x| x.unwrap().bytes().collect()).collect();
@@ -49,6 +49,36 @@ fn look_for_surrounding(arr: &Vec<Vec<u8>>, i: usize, start: usize, end: usize) 
         for col in start..end {
             let c = arr[row][col] as char;
             if !c.is_digit(10) && c != '.' {
+                found = true;
+            }
+        }
+    }
+
+    // if start > 0 && arr[i][start - 1] != b'.' {
+    //     return true;
+    // }
+    //
+    // if arr[i][end] != b'.' {
+    //     return true;
+    // }
+    found
+}
+
+fn search_and_store_gear(arr: Vec<Vec<u8>>, store: HashMap<(usize, usize), Vec<u8>>, num: u64, i: usize, start: usize, end: usize) -> bool {
+    let mut found = false;
+    for di in -1..=1 {
+        let row = i as isize + di;
+        if row < 0 || row as usize >= arr.len() {
+            continue;
+        }
+        let row = row as usize;
+
+        let start = max(0, start as isize - 1) as usize;
+        let end = min(end + 1, arr[i].len());
+        println!("{}", str::from_utf8(&arr[row][start..end]).unwrap());
+        for col in start..end {
+            let c = arr[row][col] as char;
+            if c == '*' {
                 found = true;
             }
         }
