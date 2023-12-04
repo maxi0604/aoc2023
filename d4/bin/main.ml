c1786677a0313ff7491c38481f37c755a27e3261
Maximilian R. <public@complexvector.space>
2023-12-04 19:11:20 +0100

    Finish up

--- d4/bin/main.ml
+++ d4/bin/main.ml
@@ -12,18 +12,17 @@ let number = digits >>= fun k -> (Stdlib.int_of_string_opt k) |> (function | Som
 
 let game_line = (word *> spaces *> number <* colon <* spaces) >>= fun num -> many (number <* spaces) <* pipe <* spaces >>= fun card -> many (number <* spaces) >>= fun draw -> return (num, card, draw)
 
-let hits (_, card, draw) = List.count draw (fun x -> (List.mem card x Int.equal))
+let hits (_, card, draw) = List.count draw ~f:(fun x -> (List.mem card x ~equal:Int.equal))
 let score = function
 | x when (x - 1 >= 0) -> Int.pow 2 x
 | _ -> 0
 
-let noop x = x
 let main =
     let lines = In_channel.input_lines In_channel.stdin in
     let parsed = List.map lines ~f:(Angstrom.parse_string ~consume:Prefix game_line) in
     let filtered = List.filter_map parsed ~f:Result.ok in
     let hit_list = List.map filtered ~f:hits in
-    List.sum (module Int) hit_list ~f:noop;;
+    List.sum (module Int) hit_list ~f:score;;
 
 let () =
     Stdlib.print_int main
