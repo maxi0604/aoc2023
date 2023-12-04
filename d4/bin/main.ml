35064b10e70eb08d47ea2ed358c3da21cd4273de
Maximilian R. <maxi.rostock@outlook.de>
2023-12-04 17:56:16 +0100

    Add current state into git, gotta go

--- d4/bin/main.ml
+++ d4/bin/main.ml
@@ -1,5 +1,8 @@
 open Angstrom
+open Angstrom.Consume
 open Core
+
+(**Parser**)
 let word = take 4 >>= (function | "Card" -> return () | _ -> fail "Expected 'card'")
 let spaces = take_while (function | ' ' -> true | _ -> false)
 let digits = take_while (function | '0'..'9' -> true | _ -> false)
@@ -7,7 +10,16 @@ let colon = take 1 >>| (function | ":" -> fun () -> return () | _ -> fun () -> f
 let pipe = take 1 >>| (function | "|" -> fun () -> return () | _ -> fun () -> fail "Expected '|'")
 let number = digits >>= fun k -> (Stdlib.int_of_string_opt k) |> (function | Some i -> return i | None -> fail "Expected number")
 
-let game_line = (word *> spaces *> number <* colon <* spaces) >>= fun num -> (many (number <* spaces)) <* pipe <* spaces >>= fun card -> many (number <* spaces) >>= fun draw -> return (num, card, draw)
+let game_line = (word *> spaces *> number <* colon <* spaces) >>= fun num -> many (number <* spaces) <* pipe <* spaces >>= fun card -> many (number <* spaces) >>= fun draw -> return ((num : int), (card : int list), (draw : int list))
 
-let () =
-    Stdlib.print_string "ocaml my caml";;
+let hits (_, card, draw) = List.count draw (fun x -> (List.mem card x Int.equal))
+let score x = function
+| x when (x - 1 >= 0) -> Int.pow 2 x
+| _ -> 0
+
+let main =
+    let lines = In_channel.input_lines In_channel.stdin in
+    let parsed = List.map lines ~f:(Angstrom.parse_string ~consume:Prefix game_line) in
+    let filtered = List.filter_map parsed Result.ok in
+    let hit_list = List.map filtered hits in
+    List.fold_left hit_list (fun acc x -> acc + x) hit_list 0;;
