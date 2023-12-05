acbd61f451cad0c2e6cee1cb1ce5872a5c081e1a
Maximilian R. <public@complexvector.space>
2023-12-05 19:36:34 +0100

    bad non-solution trying to keep (begin, len) representation.
    todo: dumb

--- d5/Program.cs
+++ d5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using System.Diagnostics;
+using static System.Math;
 
 readonly record struct Mapping (
     string Source,
@@ -17,6 +18,7 @@ class Program {
             Part1();
         }
     }
+
     public static void Part1() {
         var lines = Console.In.ReadToEnd().Split(Environment.NewLine);
         long[] seeds = lines[0].Split(":")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => long.Parse(x)).ToArray();
@@ -89,22 +91,56 @@ class Program {
             }
         }
 
+        for (int i = 0; i < mappings.Count; ++i) {
+            (var str, var list) = mappings[i];
+            mappings[i] = (str, list.OrderBy(x => x.Item2).ToList());
+
+        }
         long min = long.MaxValue;
         foreach (long[] seedPair in seedPairs) {
-            long cur = seed;
-
+            List<long[]> cur = new List<long[]>();
+            cur.Add(seedPair);
             foreach((_, List<(long, long, long)> list) in mappings) {
-                foreach ((long dstI, long srcI, long len) in list) {
-                    if (cur >= srcI && cur < srcI + len) {
-                        Console.WriteLine($"{cur} -> {cur - srcI + dstI}");
-                        cur = cur - srcI + dstI;
-                        break;
+                List<long[]> next = new List<long[]>();
+                foreach (long[] curPair in cur) {
+                    Console.WriteLine($"mapping {curPair[0]}..{curPair[0] + curPair[1]}.");
+                    foreach ((long dstI, long srcI, long len) in list) {
+                        // represented excluding right including left bound.
+                        long curLeft = curPair[0];
+                        long curRight = curPair[0] + curPair[1];
+                        long mapLeft = srcI;
+                        long mapRight = srcI + len;
+                        // In (left, length) notation again.
+                        long[] left = new long[] {curLeft, mapLeft - curLeft};
+                        long[] toMap = new long[] {Max(curLeft, mapLeft), Min(curRight, mapRight) - Max(curLeft, mapLeft) };
+                        long newLeft = Min(curRight, mapRight) + 1;
+                        long newCurLen = curPair[1] - Max(left[1], 0) - Max(toMap[1], 0);
+
+                        if (left[1] > 0)
+                            next.Add(left);
+
+                        Console.WriteLine($"splitting {curPair[0]}..{curPair[1] + curPair[0]} into left: {left[0]}..{left[0] + left[1]}, map: {toMap[0]}..{toMap[0] + toMap[1]} -> {}, right: {newLeft}..{newLeft + newCurLen}");
+                        if (toMap[1] > 0) {
+                            toMap[0] = toMap[0] - srcI + dstI;
+                        }
+
+                        curPair[0] = newLeft;
+                        curPair[1] = newCurLen;
+                    }
+
+                    if (curPair[1] > 0) {
+                        next.Add(curPair);
                     }
                 }
+                cur = next;
             }
-            if (cur < min) {
-                min = cur;
+
+            for (int i = 0; i < cur.Count; i++) {
+                if (cur[i][0] < min) {
+                    min = cur[i][0];
+                }
             }
+
             //Console.WriteLine($"mapped seed {seed} -> plot {cur}");
         }
         Console.WriteLine($"min: {min}");
