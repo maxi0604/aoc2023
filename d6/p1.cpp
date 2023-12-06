45b6619e43beb6187f6a6334da6d3341cedfe2e2
Maximilian <maxi.rostock@outlook.de>
2023-12-06 21:26:35 +0100

    Finish d6

---
+++ d6/p1.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <sstream>
+#include <string>
+#include <iostream>
+int main (int argc, char *argv[]) {
+    std::string times;
+    std::string dists;
+    std::getline(std::cin, times);
+    std::getline(std::cin, dists);
+    std::cin >> dists;
+
+    auto tstream = std::stringstream(times);
+    auto dstream = std::stringstream(dists);
+
+    std::string waste;
+    long t, d;
+    
+    tstream >> waste;
+    dstream >> waste;
+    long prod = 1;
+    while (tstream >> t) {
+        dstream >> d;
+        std::cout << "t: " << t << " d: " << d << std::endl;
+        long ct = 0;
+        for (long i = 0; i <= t; i++) {
+            long x = i * (t - i);
+
+            if (x > d)
+                ct++;
+        }
+        std::cout << "ct:" << ct << std::endl;
+        prod *= ct;
+    }
+
+    std::cout << "prod: " << prod << std::endl;
+
+    return 0;
+}   
