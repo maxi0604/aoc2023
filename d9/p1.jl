eebc367c0c325b6b75034a6a78f2b12dd905151e
Maximilian R. <maxi.rostock@outlook.de>
2023-12-11 12:52:18 +0100

    Less debug output

--- d9/p1.jl
+++ d9/p1.jl
@@ -4,7 +4,6 @@ function calc_line(line::AbstractString)
     values = map((x) -> parse(Int, x), split(line, " "; keepempty=false))
     for i ∈ range(0, length(values) - 1)
         for j ∈ range(1, length(values) - i - 1)
-            println(i, j)
             values[j] = values[j + 1] - values[j]
         end
         println(values)
--- d9/p2.jl
+++ d9/p2.jl
@@ -4,7 +4,6 @@ function calc_line(line::AbstractString)
     values = map((x) -> parse(Int, x), split(line, " "; keepempty=false))
     for i ∈ range(2, length(values))
         for j ∈ range(length(values), i; step=-1)
-            println(i, j)
             values[j] = values[j - 1] - values[j]
         end
         println(values)
