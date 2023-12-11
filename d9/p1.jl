eb991e7c7c0082eebeea0db59766ddc0af502e28
Maximilian R. <maxi.rostock@outlook.de>
2023-12-11 01:09:28 +0100

    Remove unused variable

--- d9/p1.jl
+++ d9/p1.jl
@@ -3,11 +3,9 @@ lines = readlines()
 function calc_line(line::AbstractString)
     values = map((x) -> parse(Int, x), split(line, " "; keepempty=false))
     for i ∈ range(0, length(values) - 1)
-        nonzero = false
         for j ∈ range(1, length(values) - i - 1)
             println(i, j)
             values[j] = values[j + 1] - values[j]
-            nonzero = nonzero || (values[j] ≠ 0)
         end
         println(values)
     end
--- d9/p2.jl
+++ d9/p2.jl
@@ -3,11 +3,9 @@ lines = readlines()
 function calc_line(line::AbstractString)
     values = map((x) -> parse(Int, x), split(line, " "; keepempty=false))
     for i ∈ range(2, length(values))
-        nonzero = false
         for j ∈ range(length(values), i; step=-1)
             println(i, j)
             values[j] = values[j - 1] - values[j]
-            nonzero = nonzero || (values[j] ≠ 0)
         end
         println(values)
     end
