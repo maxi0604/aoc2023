5b448f41831f903eaeaf5d04a71cff2a2d92ef45
Maximilian R. <public@complexvector.space>
2023-12-14 00:21:55 +0100

    Update typescript things

--- d12/main.ts
+++ d12/main.ts
@@ -1,2 +1,18 @@
 import * as rl from 'readline';
 
+const term = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+let lines: string[] = [];
+term.on("line", (input: string) => { lines.push(input) });
+term.on("close", () => {
+    console.log("p1");
+    for (const line of lines) {
+        const split = line.trim().split(" ").filter(i => i);
+        const nums = split[1].split(",").map(x => Number(x));
+        const springs = split[0];
+        console.log(nums, springs);
+    }
+});
