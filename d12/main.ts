b3f72ed5ebad7cd029f92b2651c25a035554231a
Maximilian R. <public@complexvector.space>
2023-12-14 01:02:17 +0100

    Update to prepare for using this fun thing

--- d12/main.ts
+++ d12/main.ts
@@ -5,14 +5,37 @@ const term = rl.createInterface({
     output: process.stdout,
 })
 
-let lines: string[] = [];
-term.on("line", (input: string) => { lines.push(input) });
-term.on("close", () => {
+function checkFit(field: string, len: number, idx: number) {
+    return !!field.slice(Math.max(idx - 1, 0), Math.min(idx + len + 1, field.length)).match(/^[.?][#?]*[.?]?$/);
+}
+
+function recurse(nums: number[], field: string, idx: number) {
+    if (nums.length == 0) {
+        return 1;
+    }
+
+    let count = 0;
+    for (let i = idx; i < field.length; ++i) {
+        if (checkFit(field, nums[0], i)) {
+            let sub = recurse(nums.slice(1), field, i + nums[0] + 1);
+            console.log(`counting ${sub} at ${i} with len = ${nums[0]}`);
+            count += sub;
+        }
+    }
+    return count;
+}
+
+function doTheThing(lines: string[]) {
     console.log("p1");
     for (const line of lines) {
         const split = line.trim().split(" ").filter(i => i);
         const nums = split[1].split(",").map(x => Number(x));
         const springs = split[0];
         console.log(nums, springs);
+        console.log(recurse(nums, springs, 0));
     }
-});
+}
+
+let lines: string[] = [];
+term.on("line", (input: string) => { lines.push(input) });
+term.on("close", () => doTheThing(lines));
